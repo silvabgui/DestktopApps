f948fc2cf8a42018639baec5d88d8fdda7f4a00b
silvabgui <gui_knot99@hotmail.com>
2025-04-26 15:31:43 -0300

    primeiro commit

---
+++ Projeto Cinemark/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Projeto_Cinemark
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Faturamento = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Faturamento
+            // 
+            this.btn_Faturamento.Location = new System.Drawing.Point(100, 732);
+            this.btn_Faturamento.Name = "btn_Faturamento";
+            this.btn_Faturamento.Size = new System.Drawing.Size(201, 82);
+            this.btn_Faturamento.TabIndex = 0;
+            this.btn_Faturamento.Text = "Faturamento";
+            this.btn_Faturamento.UseVisualStyleBackColor = true;
+            this.btn_Faturamento.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 826);
+            this.Controls.Add(this.btn_Faturamento);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Faturamento;
+    }
+}
+
---
+++ Projeto Cinemark/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_Cinemark
+{
+    public partial class Form1 : Form
+    {
+        Button[,] b = new Button[10, 20];
+        char letra = 'A';
+        int ocupadas = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    b[i, j] = new Button();
+                    b[i, j].Name = "Botão " + (i + 1);
+                    b[i, j].Text = letra + "" + (j + 1);
+                    b[i, j].Location = new Point(100 + j * 80, 60 + i * 60);
+                    b[i, j].BackColor = Color.Green;
+                    b[i, j].ForeColor = Color.White;
+                    b[i, j].Size = new Size(76, 57);
+                    Controls.Add(b[i, j]);
+                    b[i, j].Click += metodoGenerico;
+                }
+                letra++;
+            }
+        }
+
+        private void metodoGenerico(object sender, EventArgs e)
+        {
+            Button aux = (Button)sender;
+            if (aux.BackColor == Color.Green)
+            {
+                DialogResult BoxResponse = MessageBox.Show(
+                    "Deseja selecionar a Poltrona " + aux.Text + "?",
+                    "Confirmação",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question
+                );
+
+                if (BoxResponse == DialogResult.Yes)
+                {
+                    aux.BackColor = Color.Red;
+                    ++ocupadas;
+                }
+            }
+
+            else if (aux.BackColor == Color.Red)
+            {
+
+                DialogResult boxResponse = MessageBox.Show(
+                    "Deseja cancelar a reserva ?",
+                    "Confirmação",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question
+                );
+
+                aux.BackColor = Color.Green;
+                --ocupadas;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int vlrTotal = ocupadas * 20;
+            MessageBox.Show("Valor Total: R$ " + vlrTotal, "Ingresso(s)");
+        }
+    }
+}
